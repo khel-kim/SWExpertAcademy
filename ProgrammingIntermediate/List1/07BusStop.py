918ea85f0a5d9a016cd9aa370d438b44364cfb1b
Khel-Kim <cafehero123@gmail.com>
2019-05-30 10:19:14 +0900

    solve Bus stop

--- ProgrammingIntermediate/List1/07BusStop.py
+++ ProgrammingIntermediate/List1/07BusStop.py
@@ -4,11 +4,31 @@ for _ in range(T):
     k, n, m = map(int, input().split())
     station = tuple(map(int, input().split()))
     data.append((k, n, m, station))
-print(data)
+
 
 def sol(arr):
-    k, n, m, station = arr[0], arr[1], arr[2], arr[3]
+    k, n, m, station = arr
     current = 0
     count = 0
-    while n <= current:
+    start = 0
+    while True:
+        now = current
         current += k
+        if current >= n:
+            return count
+        else:
+            candi = []
+            for i in station[start:]:
+                if now + 1 <= i <= current:
+                    candi.append(i)
+                    start = station.index(i) + 1
+                elif current < i:
+                    break
+        if not candi:
+            return 0
+        count += 1
+        current = candi[-1]
+
+
+for i, arr in enumerate(data):
+    print("#%s" %(i + 1), sol(arr))
