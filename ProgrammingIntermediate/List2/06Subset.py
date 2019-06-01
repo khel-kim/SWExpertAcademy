cf7cb0673bedeb067389cf19fc993cbaf4924a6d
Khel-Kim <cafehero123@gmail.com>
2019-06-01 11:40:55 +0900

    add 06Subset

--- ProgrammingIntermediate/List2/06Subset.py
+++ ProgrammingIntermediate/List2/06Subset.py
@@ -5,12 +5,10 @@ for _ in range(T):
     n, k = list(map(int, input().split()))
     data.append((n, k))
 
-arr = data[2]
 ################## sol1
 def sol1(arr, board):
     n = arr[0]
     k = arr[1]
-
     count = 0
     for i in range(1 << 12):
         candi = []
@@ -18,10 +16,35 @@ def sol1(arr, board):
         for j in range(len(board)):
             if i & (1 << j):
                 candi.append(board[j])
-        print(candi)
         if len(candi) == n and sum(candi) == k:
             count +=1
     return count
 
 for i, arr in enumerate(data):
     print("#%s" %(i + 1), sol1(arr, board))
+
+################ sol2
+def combination(arr, m):
+    visit = []
+    def generator(arr, m, i=0):
+        if len(visit) == m:
+            yield visit
+        else:
+            for j in range(i, len(arr)):
+                if arr[j] in visit: continue
+                visit.append(arr[j])
+                yield from generator(arr, m, i=j+1)
+                visit.pop()
+    yield from generator(arr, m)
+
+def sol2(arr, baord):
+    n = arr[0]
+    k = arr[1]
+    count = 0
+    for candi in combination(board, n):
+        if sum(candi) == k:
+            count += 1
+    return count
+
+for i, arr in enumerate(data):
+    print("#%s" %(i + 1), sol2(arr, board))
