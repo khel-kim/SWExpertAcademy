a13ad56af633afddd26b96032a3998eab9fddac8
Khel Kim <cafehero123@gmail.com>
2019-07-16 14:52:19 +0900

    mkdir practice

---
+++ Practice/1938.py
@@ -0,0 +1,11 @@
+number1, number2 = list(map(int, input().split()))
+
+
+def sol(number1, number2):
+    print(number1 + number2)
+    print(number1 - number2)
+    print(number1 * number2)
+    print(int(number1 / number2))
+
+
+sol(number1, number2)
