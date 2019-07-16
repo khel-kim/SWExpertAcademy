3f0ee3dd51a03ddd71329fca88fb1d520f555d92
Khel Kim <cafehero123@gmail.com>
2019-07-16 11:19:34 +0900

    reduce the length of the code

--- ProgrammingAdvanced/03ApplicationOfDynamicProgramming/04SumOfPowerSet.py
+++ ProgrammingAdvanced/03ApplicationOfDynamicProgramming/04SumOfPowerSet.py
@@ -14,14 +14,14 @@ def sol(case):
     for i in range(1, n + 1):
         for j in range(1, k + 1):
             check = j - i
-            if check < 0:
-                R[i][j] += R[i - 1][j]
+            R[i][j] += R[i - 1][j]
+            if check < 0: continue
             elif check == 0:
                 R[i][j] += 1
-                R[i][j] += R[i - 1][j]
+                continue
             elif check > 0:
                 R[i][j] += R[i - 1][check]
-                R[i][j] += R[i - 1][j]
+                continue
     return R[-1][-1]
 
 
