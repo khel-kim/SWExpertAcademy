5d793d9843a833825d29114af9777124ae3d7583
Khel Kim <cafehero123@gmail.com>
2019-07-17 18:21:13 +0900

    add all pairs shortestpath

--- untitle.py
+++ untitle.py
@@ -1,11 +1,4 @@
+import collections
 blood_types_str = "A B A O AB AB O A B O B AB"
 blood_types = blood_types_str.split()
-print(blood_types)
-
-dict = {}
-for blood in blood_types:
-    if blood in dict.keys():
-        dict[blood] += 1
-    else:
-        dict[blood] = 1
-print(dict)
+print(collections.Counter(blood_types))
